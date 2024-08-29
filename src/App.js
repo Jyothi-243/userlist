80185ac5f142f5eb3f7e3d0f02e8ea6c5c8bb246
Jyothi Priya <jyothi.priya@vedantu.com>
2024-08-29 21:38:14 +0530

    userlist component added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import UserProfile from "./components/UserProfile";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const userDetailsList =[ {
+  uniqueNo:1,
+  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name:"priya",
+  role: "intern"
+},
+{
+  uniqueNo:2,
+  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name:"padma",
+  role: "intern"
+},
+{
+  uniqueNo:3,
+  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name:"sravya",
+  role: "intern"
+},
+{
+  uniqueNo:4,
+  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name:"chakritha",
+  role: "intern"
 }
+];
 
+// to pass this as prop
+const App = () =>(
+  <div className="list-container">
+    <h1 className="title">Users List</h1>
+    <input type="search"/>
+    <ul>
+      {userDetailsList.map((eachItem) => (
+        <UserProfile UserDetails={eachItem} key={eachItem.uniqueNo}/>
+      ))}
+    </ul>
+
+  </div>
+)
 export default App;
+
+
+
+//<UserProfile userDetails={userDetailsList}/> //renders user profile\
+//keys:keys help react to identify which items have changed,added or removed.
+//key doesnot come in components.we cant access key in props.
